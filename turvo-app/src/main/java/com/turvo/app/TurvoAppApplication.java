509d38c4a1e5fe70d883ead022bec6d44636cb37
Dheeraj Vats <dheeraj.djava@gmail.com>
2019-09-23 14:37:51 +0530

    Initial Commit

---
+++ turvo-app/src/main/java/com/turvo/app/TurvoAppApplication.java
@@ -0,0 +1,13 @@
+package com.turvo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurvoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurvoAppApplication.class, args);
+	}
+
+}
